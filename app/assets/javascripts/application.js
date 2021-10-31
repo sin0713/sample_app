9e39b5edc4f14f73535612ad125b1b634d8a0a5b
sin0713 <narisin1124@gmail.com>
2021-10-31 11:05:35 +0000

    [Add]rspec

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,14 +22,14 @@ var window_h = $(window).height();
 
 //スクロールイベント
 $(window).on("scroll", function() {
-  
+
   //スクロールの位置を取得する
   var scroll_top = $(window).scrollTop();
- 
+
   $(".list-box").each(function() {
     //各box要素のtopの位置を取得する
     var elem_pos = $(this).offset().top;
-    
+
     //どのタイミングでフェードインさせるか
     if (scroll_top >= elem_pos - window_h + 300) {
       $(this).addClass("fadein");　//特定の位置を超えたらクラスを追加
@@ -38,3 +38,4 @@ $(window).on("scroll", function() {
     }
   });
 });
+
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,5 @@
 class List < ApplicationRecord
   attachment :image
-  validates :image_id, presence: true
   validates :title, presence: true
   validates :body, presence: true
 end
